0dd0114eaaf70b72c5441531f365cddbca8f3de7
Ricardo Torres <ricardo@yardim.com.br>
2022-03-03 12:03:03 -0300

    #3 ok

--- app/test/index.js
+++ app/test/index.js
@@ -1,7 +1,10 @@
 const { expect } = require('chai');
 const chai = require('chai')
-//TDD com Node #02 - Fazendo Testes Unitários
-const somarNumeros = (a, b) => a + b
+
+const somarNumeros = (a, b) => {
+    if (typeof a === "number" && typeof b === "number") return a + b;
+    else return undefined;
+}
 
 describe('Somas', () => {
     it ('Soma de dois numeros - 2 e 3', (done) => {
@@ -13,6 +16,12 @@ describe('Somas', () => {
     it ('Soma de dois numeros - -2 e 3', (done) => {
         const resultado = somarNumeros(-2,3);
         expect(resultado).be.equal(1);
+        done();
+    });
+
+    it ('Soma de dois numeros - "teste" e 3', (done) => {
+        const resultado = somarNumeros("teste",3);
+        expect(resultado).be.equal(undefined);
         done();
     });
 });
